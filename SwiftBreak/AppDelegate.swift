3cdab558136ec41159e1eda6c9c18143719a7f20
Luís Silva <lm2s@gmx.com>
2020-11-19 14:05:35 +0000

    Initial Commit

---
+++ SwiftBreak/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftBreak
+//
+//  Created by Luís Silva on 19/11/2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
